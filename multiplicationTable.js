2fef2ad588def010c69b9e1dce566377d3c964a8
Nasir Ahmed <nasirahmed595@gmail.com>
2022-01-30 01:11:04 +0600

    first commit

---
+++ multiplicationTable.js
@@ -0,0 +1,55 @@
+// ******************************
+// ****Convert to lower case*****
+// ******************************
+
+function multiplicationTable(number) {
+    for (var i = 1; i <= 10; i++) {
+        var result = i * number;
+        console.log(i, '*', number, '=', result);
+
+    }
+}
+var num1 = 29;
+multiplicationTable(num1);
+
+
+// ******************************
+// ****Convert to lower case*****
+// ******************************
+function lowerCase(pera) {
+    // console.log("It worked!");
+    var result = pera.toLowerCase();
+    return result;
+}
+var inputFun = 'KI R KOMU DUKKHER KOTHA! LOWERCASE KORTE PARTACHI NA. ';
+
+var returnFun = lowerCase(inputFun);
+
+console.log("LOWER CASE OF YOUR INPUT: ", returnFun);
+
+// **********************************
+// *******Full Name Function ********
+// **********************************
+
+function fullNameFun(firstName, lastName) {
+    var fullName = firstName + " " + lastName;
+    return fullName;
+}
+
+var result = fullNameFun("Nasir", "Ahmed")
+
+console.log(result);
+
+
+
+// **********************************
+// *******Squar Function ********
+// **********************************
+
+function squarFun(num) {
+    var squarResult = num * num;
+    return squarResult;
+}
+var number = 4;
+
+console.log(squarFun(number));
